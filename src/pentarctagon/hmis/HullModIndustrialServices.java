75a50474de8fb9c2953912e950bc5dfc42d518d0
pentarctagon <pentarctagon@tutamail.com>
2025-11-29 18:06:55 -0600

    remove built-in button from refit screen

--- src/pentarctagon/hmis/HullModIndustrialServices.java
+++ src/pentarctagon/hmis/HullModIndustrialServices.java
@@ -1,10 +1,89 @@
 package pentarctagon.hmis;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.EveryFrameScript;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.listeners.ListenerManagerAPI;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 @SuppressWarnings("unused")
 public class HullModIndustrialServices
 extends BaseModPlugin
 {
+	private static final String[] reflectionWhitelist = new String[] {
+		"pentarctagon.hmis.data.campaign.rulecmd.ui.refit.RefitTabListenerAndScript",
+		"pentarctagon.hmis.data.campaign.rulecmd.ui.refit.ReflectionStuff"
+	};
 
+	public static ReflectionEnabledClassLoader getClassLoader()
+	{
+		URL url = HullModIndustrialServices.class.getProtectionDomain().getCodeSource().getLocation();
+		return new ReflectionEnabledClassLoader(url, HullModIndustrialServices.class.getClassLoader());
+	}
+
+	@Override
+	public void onGameLoad(boolean newGame)
+	{
+		try
+		{
+			Class<?> cls = getClassLoader().loadClass("pentarctagon.hmis.data.campaign.rulecmd.ui.refit.RefitTabListenerAndScript");
+			MethodHandles.Lookup lookup = MethodHandles.lookup();
+			MethodHandle mh = lookup.findConstructor(cls, MethodType.methodType(void.class));
+			EveryFrameScript refitScript = (EveryFrameScript) mh.invoke();
+			ListenerManagerAPI listeners = Global.getSector().getListenerManager();
+			listeners.addListener(refitScript, true);
+			Global.getSector().addTransientScript(refitScript);
+		}
+		catch(Throwable e)
+		{
+			throw new RuntimeException("Failed to add refit tab listener", e);
+		}
+	}
+
+	public static class ReflectionEnabledClassLoader
+	extends URLClassLoader
+	{
+		public ReflectionEnabledClassLoader(URL url, ClassLoader parent)
+		{
+			super(new URL[]{url}, parent);
+		}
+
+		@Override
+		public Class<?> loadClass(String name)
+		throws ClassNotFoundException
+		{
+			if(name.startsWith("java.lang.reflect"))
+			{
+				return ClassLoader.getSystemClassLoader().loadClass(name);
+			}
+			return super.loadClass(name);
+		}
+
+		@Override
+		public Class<?> loadClass(String name, boolean resolve)
+		throws ClassNotFoundException
+		{
+			Class<?> c = findLoadedClass(name);
+			if(c != null)
+			{
+				return c;
+			}
+			// Be the defining classloader for all classes in the reflection whitelist
+			// For classes defined by this loader, classes in java.lang.reflect will be loaded directly
+			// by the system classloader, without the intermediate delegations.
+			for(String str : reflectionWhitelist)
+			{
+				if(name.startsWith(str))
+				{
+					return findClass(name);
+				}
+			}
+			return super.loadClass(name, resolve);
+		}
+	}
 }
--- src/pentarctagon/hmis/data/campaign/rulecmd/HMIS_ManageHullMods.java
+++ src/pentarctagon/hmis/data/campaign/rulecmd/HMIS_ManageHullMods.java
@@ -10,11 +10,17 @@ import pentarctagon.hmis.data.campaign.rulecmd.utils.Sizing;
 import java.util.List;
 import java.util.Map;
 
-// TODO: remove build in button from refit screen
+// TODO: review/document how the code removing the refit button works
+// TODO: check that the pointless vanity project achievement triggers
+// TODO: removing s-mods costs credits instead of a story point?
+// TODO: require story point improvement to enable going beyond 2 s-mods?
+// TODO: reduce d-mod removal costs - need to look at SMS
+// TODO: console commands?
 // TODO: lunalib integration - update URLs in version file
 // TODO: somehow prevent gaming the costs by increasing faction doctrine quality -> s-mod stuff -> decrease it?
 // TODO: logging?
-// TODO: how to handle best of the best skill
+// TODO: how to handle best of the best skill?
+// TODO: parade piece explosion radius+effects
 
 /**
  * Entry point for hull mod management dialog - see rules.csv
